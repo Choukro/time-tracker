941701e23d607e21ce261a667a84c6145beb81ff
Pierre-Henri Merrer <phmerrer@gmail.com>
2023-11-01 22:58:11 +0100

    style (Application title) : adjust padding

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex flex-col justify-between p-10">
       <div className="flex flex-col gap-4 max-w-4xl m-auto">
-        <h1 className="font-extrabold text-transparent md:text-8xl sm:text-6xl text-4xl leading-tight	bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        <h1 className="font-extrabold text-transparent md:pb-10 pb-2 md:text-8xl sm:text-6xl text-4xl leading-tight	bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
           Time Tracker
         </h1>
       </div>
