bdc57c62178c2d70b765bb914ce608a1b588d96e
Pierre-Henri Merrer <phmerrer@gmail.com>
2023-11-01 17:14:07 +0100

    fix (useLocalStorage) : ReferenceError: window is not defined

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -3,19 +3,26 @@ import { isFunction } from "../utils/trackers.util.js";
 
 const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = React.useState(() => {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
+    if (typeof window !== "undefined") {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    }
+    return initialValue;
   });
+
   const setValue = React.useCallback(
     (value) => {
-      setStoredValue(value);
-      window.localStorage.setItem(
-        key,
-        JSON.stringify(isFunction(value) ? value(storedValue) : value)
-      );
+      if (typeof window !== "undefined") {
+        setStoredValue(value);
+        window.localStorage.setItem(
+          key,
+          JSON.stringify(isFunction(value) ? value(storedValue) : value)
+        );
+      }
     },
     [key, storedValue]
   );
+
   return [storedValue, setValue];
 };
 
